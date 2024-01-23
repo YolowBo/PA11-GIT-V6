84607e969b53f8aa6de2f276ebca350f4670dab7
YolowBo <155929975+YolowBo@users.noreply.github.com>
2024-01-23 15:48:24 +0800

    Add Unity Project

---
+++ 12_TayBoWen_Lab10/Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public Camera MainCamera;
+    private Vector3 screenbound;
+    private float ObjectW;
+    private float ObjectH;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        screenbound = MainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, MainCamera.transform.position.z));
+        ObjectW = transform.GetComponent<MeshRenderer>().bounds.extents.x;
+        ObjectH = transform.GetComponent<MeshRenderer>().bounds.extents.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.position = transform.position + new Vector3(0 , verticalInput * speed * Time.deltaTime, 0);
+
+        Vector3 viewPos = transform.position;
+        viewPos.y = Mathf.Clamp(viewPos.y, screenbound.y * -1 + ObjectH, screenbound.y - ObjectH);
+
+        transform.position = viewPos;
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("LoseScene");
+        }
+    }
+}
---
+++ 12_TayBoWen_Lab10/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScoreScript : MonoBehaviour
+{
+    [SerializeField]Text ScoreTxt;
+    int Score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Obstacle")
+        {
+            Score++;
+            ScoreTxt.text = "Score: " + Score;
+           
+        }
+    }
+}
